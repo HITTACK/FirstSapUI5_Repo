d54cdbee772fb10ec4979cffea7202c4e729e94c
Harshit Jain <harshit.jain@ingenxtec.com>
2026-08-19 07:11:13 +0000

    dynamic ui element's properties change by getter and setter

--- firstsapui5/webapp/controller/View1.controller.js
+++ firstsapui5/webapp/controller/View1.controller.js
@@ -8,6 +8,21 @@ sap.ui.define([
         },
         onPress: function () {
             this.getOwnerComponent().getRouter().navTo("RouteView2");
+        },
+        onSubmit:function () {
+            //get the current value by getter method
+            var name = this.getView().byId("idInputName").getValue();
+            var msg = "Welcome To " + name;
+
+            // set the new value into text by setter method
+            this.getView().byId("idTextWelcome").setText(msg);
+
+            //set the colour of the button as green after click
+            this.getView().byId("idSubmit").setType("Accept");
+        },
+        onBack:function () {
+            this.getView().byId("idTextWelcome").setText("Welcome to SAPUI5");
+            this.getView().byId("idInputName").setValue("");
         }
     });
 });
